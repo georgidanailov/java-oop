4e63185d17ea186111af699a4aafcbbe82c6e4e2
Georgi Danailov <70630746+demonhandz@users.noreply.github.com>
2023-12-09 21:42:28 +0200

    Update

--- src/abstraction/greedyTimes/Main.java
+++ src/abstraction/greedyTimes/Main.java
@@ -6,92 +6,84 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+    private static final String GOLD = "Gold";
+    private static final String GEM = "Gem";
+    private static final String CASH = "Cash";
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long bagCapacity = Long.parseLong(scanner.nextLine());
         String[] items = scanner.nextLine().split("\\s+");
-
         LinkedHashMap<String, LinkedHashMap<String, Long>> bag = new LinkedHashMap<>();
-        long gold = 0;
-        long rocks = 0;
-        long money = 0;
 
         for (int i = 0; i < items.length; i += 2) {
             String name = items[i];
             long count = Long.parseLong(items[i + 1]);
 
-            String item = "";
+            String itemCategory = categorizeItem(name);
 
-            if (name.length() == 3) {
-                item = "Cash";
-            } else if (name.toLowerCase().endsWith("gem")) {
-                item = "Gem";
-            } else if (name.toLowerCase().equals("gold")) {
-                item = "Gold";
-            }
-
-            if (item.equals("")) {
-                continue;
-            } else if (bagCapacity < bag.values().stream().map(Map::values).flatMap(Collection::stream).mapToLong(e -> e).sum() + count) {
+            if (itemCategory.equals("") || exceedsBagCapacity(bag, bagCapacity, count)) {
                 continue;
             }
-
-            switch (item) {
-                case "Gem":
-                    if (!bag.containsKey(item)) {
-                        if (bag.containsKey("Gold")) {
-                            if (count > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else if (bag.get(item).values().stream().mapToLong(e -> e).sum() + count > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                        continue;
-                    }
-                    break;
-                case "Cash":
-                    if (!bag.containsKey(item)) {
-                        if (bag.containsKey("Gem")) {
-                            if (count > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else if (bag.get(item).values().stream().mapToLong(e -> e).sum() + count > bag.get("Gem").values().stream().mapToLong(e -> e).sum()) {
+            switch (itemCategory) {
+                case GEM:
+                case CASH:
+                    if (!isValidToAdd(bag, itemCategory, count)) {
                         continue;
                     }
                     break;
             }
 
-            if (!bag.containsKey(item)) {
-                bag.put((item), new LinkedHashMap<>());
-            }
-
-            if (!bag.get(item).containsKey(name)) {
-                bag.get(item).put(name, 0L);
-            }
-
-
-            bag.get(item).put(name, bag.get(item).get(name) + count);
-            if (item.equals("Gold")) {
-                gold += count;
-            } else if (item.equals("Gem")) {
-                rocks += count;
-            } else if (item.equals("Cash")) {
-                money += count;
-            }
+            addToBag(bag, name, itemCategory, count);
         }
 
-        for (var x : bag.entrySet()) {
-            Long sumValues = x.getValue().values().stream().mapToLong(l -> l).sum();
+        printBagContents(bag);
+    }
 
-            System.out.println(String.format("<%s> $%s", x.getKey(), sumValues));
+    private static String categorizeItem(String name) {
+        if (name.length() == 3) {
+            return CASH;
+        } else if (name.toLowerCase().endsWith("gem")) {
+            return GEM;
+        } else if (name.toLowerCase().equals("gold")) {
+            return GOLD;
+        }
+        return "";
+    }
 
-            x.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey())).forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
+    private static boolean exceedsBagCapacity(Map<String, LinkedHashMap<String, Long>> bag, long capacity, long count) {
+        return capacity < bag.values().stream().map(Map::values)
+                .flatMap(Collection::stream).mapToLong(e -> e).sum() + count;
+    }
 
+    private static boolean isValidToAdd(Map<String, LinkedHashMap<String, Long>> bag, String itemType, long count) {
+        if (!bag.containsKey(itemType)) {
+            return bag.containsKey(GOLD) && count <= sumValuesOfItemType(bag, GOLD);
+        } else {
+            if(itemType.equals(GEM)) {
+                return sumValuesOfItemType(bag, GEM) + count <= sumValuesOfItemType(bag, GOLD);
+            }else {
+                return sumValuesOfItemType(bag, CASH) + count <= sumValuesOfItemType(bag, GEM);
+            }
+        }
+    }
+
+    private static long sumValuesOfItemType(Map<String, LinkedHashMap<String, Long>> bag, String itemType) {
+        return bag.get(itemType).values().stream().mapToLong(Long::longValue).sum();
+    }
+
+    private static void addToBag(Map<String, LinkedHashMap<String, Long>> bag, String name, String itemType, long count) {
+        bag.putIfAbsent(itemType, new LinkedHashMap<>());
+        bag.get(itemType).put(name, bag.get(itemType).getOrDefault(name, 0L) + count);
+    }
+
+    private static void printBagContents(Map<String, LinkedHashMap<String, Long>> bag){
+        for (var entry : bag.entrySet()) {
+            Long sumValues = sumValuesOfItemType(bag, entry.getKey());
+            System.out.println(String.format("<%s> $%s", entry.getKey(), sumValues));
+            entry.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey()))
+                    .forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
         }
     }
 }

84d2c4c60f234fff2eb2b9b73c031b06edc2a7a6
demonhandz <demonhandz@gmail.com>
2024-04-20 00:15:01 +0300

    Beverage part of the task is done, still need to complete the Food task requirements.

---
+++ src/inheritance/restaurant/Beverage/Tea.java
@@ -0,0 +1,9 @@
+package inheritance.restaurant.Beverage;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}

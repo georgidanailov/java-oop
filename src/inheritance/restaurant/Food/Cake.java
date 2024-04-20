859fcc3f3ae52bd91aad6fef396a9e36e8661a6a
demonhandz <demonhandz@gmail.com>
2024-04-20 15:44:54 +0300

    Task completed

---
+++ src/inheritance/restaurant/Food/Cake.java
@@ -0,0 +1,2 @@
+package inheritance.restaurant.Food;public class Cake {
+}
--- src/inheritance/restaurant/Product.java
+++ src/inheritance/restaurant/Product.java
@@ -3,11 +3,12 @@ package inheritance.restaurant;
 import java.math.BigDecimal;
 
 public class Product {
-    private String name;
-    private BigDecimal price;
+    String name;
+    BigDecimal price;
 
     public Product(String name, BigDecimal price) {
-
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
